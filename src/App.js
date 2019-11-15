6212b709a9ef0989a9b8700aa2bc7815f5a4e10c
pmanlig <spamfilter@manlig.org>
2019-11-15 21:58:50 +0100

    Working application

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router basename={`${process.env.PUBLIC_URL}`}>
+			<div className="App">
+				<header className="App-header">
+					<h1>Advent of Code 2019</h1>
+				</header>
+				<Switch>
+					<Route exact path="/" component={p => <p>Heyyy!</p> } />
+					<Route path="/" component={p => <h2>404 - this puzzle does not exist!</h2> } />
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
 export default App;

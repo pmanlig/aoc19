aa90bcca506a1e4f4b683241b57882c4f95c49ef
pmanlig <patrik@manlig.org>
2019-11-17 00:17:48 +0100

    Updates

---
+++ precache-manifest.26a4a2ebf643950a804c8c6577e90f5a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44d9f52f382b7ac0fdd816a85f9d30f4",
+    "url": "/aoc19/index.html"
+  },
+  {
+    "revision": "26e2f7618c756c4e535b",
+    "url": "/aoc19/static/css/main.ea9aedb6.chunk.css"
+  },
+  {
+    "revision": "0b44a79fee789ca2f967",
+    "url": "/aoc19/static/js/2.66416352.chunk.js"
+  },
+  {
+    "revision": "26e2f7618c756c4e535b",
+    "url": "/aoc19/static/js/main.93fe6d21.chunk.js"
+  },
+  {
+    "revision": "69dc9c949f9440d7d576",
+    "url": "/aoc19/static/js/runtime-main.0e9c9967.js"
+  }
+]);

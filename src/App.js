bf4512d9e694ac9aaf8cf1d4a78aab35cb2ca4dc
pmanlig <patrik@manlig.org>
2019-11-17 00:25:39 +0100

    HashRouting

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import { HashRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import { Grid } from './Grid';
 import { Solution } from './Solution';
 import {
--- src/AppHeader.js
+++ src/AppHeader.js
@@ -5,8 +5,8 @@ export default class AppHeader extends React.Component {
 	render() {
 		let day = this.props.day;
 		return <header className="App-header">
-			{day && day > 1 && <input className="prev" type="button" value="< Previous" onClick={e => { e.stopPropagation(); window.location.href = `/${day - 1}`; }} />}
-			{day && day < 25 && <input className="next" type="button" value="Next >" onClick={e => { e.stopPropagation(); window.location.href = `/${day + 1}`; }} />}
+			{day && day > 1 && <input className="prev" type="button" value="< Previous" onClick={e => { e.stopPropagation(); window.location.href = `/#${day - 1}`; }} />}
+			{day && day < 25 && <input className="next" type="button" value="Next >" onClick={e => { e.stopPropagation(); window.location.href = `/#${day + 1}`; }} />}
 			<div onClick={() => window.location.href = "/"}><h1>{day ? `Advent of Code 2019 - Day ${day}` : "Advent of Code 2019"}</h1></div>
 		</header>
 	}

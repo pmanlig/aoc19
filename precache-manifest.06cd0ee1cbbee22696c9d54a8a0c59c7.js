b2f8e23db7ea2e2cc9f8af0d5edda0c90b806a0c
pmanlig <spamfilter@manlig.org>
2019-12-07 03:04:08 +0100

    Updates

---
+++ precache-manifest.06cd0ee1cbbee22696c9d54a8a0c59c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef55c8152b7f7c54f8ab73394aa2e598",
+    "url": "/aoc19/index.html"
+  },
+  {
+    "revision": "b7b60572662d252a2e14",
+    "url": "/aoc19/static/css/main.3730f1db.chunk.css"
+  },
+  {
+    "revision": "89166b742f9bbd1cf46b",
+    "url": "/aoc19/static/js/2.8606f487.chunk.js"
+  },
+  {
+    "revision": "b7b60572662d252a2e14",
+    "url": "/aoc19/static/js/main.30158499.chunk.js"
+  },
+  {
+    "revision": "69dc9c949f9440d7d576",
+    "url": "/aoc19/static/js/runtime-main.0e9c9967.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/aoc19/precache-manifest.6f4b5296b3d244161c020d4364c94cb3.js"
+  "/aoc19/precache-manifest.06cd0ee1cbbee22696c9d54a8a0c59c7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/aoc19/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });

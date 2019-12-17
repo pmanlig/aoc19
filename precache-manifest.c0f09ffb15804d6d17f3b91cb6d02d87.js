b96d061fd805a6fd74afc78519dade023a097aa7
Manlig Patrik <pamanl@lantm.intra.local>
2019-12-17 13:54:05 +0100

    Updates

--- precache-manifest.c0f09ffb15804d6d17f3b91cb6d02d87.js
+++ precache-manifest.c0f09ffb15804d6d17f3b91cb6d02d87.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "00cf0126a349f2c66426c41d3f439ab2",
+    "revision": "b4b05ae496d11c4bed6e6edae079e95f",
     "url": "/aoc19/index.html"
   },
   {
-    "revision": "a393839638a7b1971dcb",
+    "revision": "228f9f6f60edc2d834c8",
     "url": "/aoc19/static/css/main.34aa91ee.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/aoc19/static/js/2.48748834.chunk.js"
   },
   {
-    "revision": "a393839638a7b1971dcb",
-    "url": "/aoc19/static/js/main.546d7834.chunk.js"
+    "revision": "228f9f6f60edc2d834c8",
+    "url": "/aoc19/static/js/main.8164bb71.chunk.js"
   },
   {
     "revision": "69dc9c949f9440d7d576",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/aoc19/precache-manifest.6ff64f91012db2bb79132565d1a63898.js"
+  "/aoc19/precache-manifest.c0f09ffb15804d6d17f3b91cb6d02d87.js"
 );
 
 self.addEventListener('message', (event) => {

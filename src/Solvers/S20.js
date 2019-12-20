8c089d5360f4e1232c055e1711bb1dee6e16ceac
pmanlig <patrik@manlig.org>
2019-12-20 17:03:10 +0100

    Day 20

--- src/Solvers/S20.js
+++ src/Solvers/S20.js
@@ -16,6 +16,7 @@ const south = { x: 0, y: 1 };
 const east = { x: 1, y: 0 };
 const west = { x: -1, y: 0 };
 const directions = [north, south, east, west];
+const pixel_size = 5;
 
 export class S20a extends Solver {
 	findPortals(map) {
@@ -50,10 +51,19 @@ export class S20a extends Solver {
 		return portals;
 	}
 
-	tryMove(path, dir, map, distMap, paths) {
-		let n = { x: path.x + dir.x, y: path.y + dir.y, steps: path.steps + 1 };
+	tryMove(path, dir, map, distMap, paths, recurse) {
+		let n = { x: path.x + dir.x, y: path.y + dir.y, steps: path.steps + 1, lvl: path.lvl };
 		let c = map[n.y][n.x];
 		if (typeof (c) === "object") {
+			if (recurse) {
+				if (n.x === 1 || n.y === 1 || n.x === map[n.y].length - 2 || n.y === map.length - 2) {
+					n.lvl--;
+					if (n.lvl < 0) return; // No portal from lvl 0
+				} else {
+					n.lvl++;
+					if (distMap[n.lvl] === undefined) distMap[n.lvl] = map.map(l => l.map(() => 0));
+				}
+			}
 			let newPos = c.filter(p => p.x !== n.x || p.y !== n.y)[0];
 			n.x = newPos.x;
 			n.y = newPos.y;
@@ -61,43 +71,58 @@ export class S20a extends Solver {
 			directions.forEach(d => this.tryMove(n, d, map, distMap, paths));
 			return;
 		}
-		if (c === '.' || c === 'Z') {
-			let p = distMap[n.y][n.x];
+		if (c === '.' || (c === 'Z' && n.lvl === 0)) {
+			let p = distMap[n.lvl][n.y][n.x];
 			if (p === 0 || p > n.steps) {
-				distMap[n.y][n.x] = n.steps;
+				distMap[n.lvl][n.y][n.x] = n.steps;
 				paths.push(n);
 			}
 		}
 	}
 
-	findPath(map, start, stop) {
-		let distMap = map.map(l => l.map(() => 0));
+	findPath(map, start, stop, recurse) {
+		let distMap = [];
+		distMap[0] = map.map(l => l.map(() => 0));
 		start.steps = 0;
+		start.lvl = 0;
 		let paths = [start];
-		distMap[start.y][start.x] = 0;
+		distMap[start.lvl][start.y][start.x] = 0;
 		while (paths.length > 0) {
 			let p = paths.shift();
 			if (p.x === stop.x && p.y === stop.y) {
 				return p.steps - 2;
 			}
 			directions.forEach(d => {
-				this.tryMove(p, d, map, distMap, paths);
+				this.tryMove(p, d, map, distMap, paths, recurse);
 			});
 			paths.sort((a, b) => a.steps - b.steps);
 		}
 	}
 
+	solveRecursive(map, portals) {
+		this.setState({ recurse: this.findPath(map, portals["AA"][0], portals["ZZ"][0], true) });
+	}
+
+	solveNonRecursive(map, portals) {
+		this.setState({ path: this.findPath(map, portals["AA"][0], portals["ZZ"][0], false) });
+		setTimeout(() => this.solveRecursive(map, portals), 10);
+	}
+
 	solve(input) {
 		let map = input.split("\n").map(l => l.split(""));
 		let portals = this.findPortals(map);
-		this.setState({ map: map, path: this.findPath(map, portals["AA"][0], portals["ZZ"][0]) });
+		this.setState({ map: map });
+		setTimeout(() => this.solveNonRecursive(map, portals), 10);
 	}
 
 	customRender() {
 		let i = 0;
-		return <div style={{ fontFamily: "monospace", whiteSpace: "pre" }}>
-			<p>Distance: {this.state.path}</p>
-			{this.state.map && this.state.map.map(l => <p key={i++}>{l.map(c => typeof (c) === "object" ? '@' : c).join("")}</p>)}
+		return <div>
+			<p>Distance non-recursive: {this.state.path}</p>
+			<p>Distance recursive: {this.state.recurse}</p>
+			<div style={{ fontFamily: "monospace", whiteSpace: "pre" }}>
+				{this.state.map && this.state.map.map(l => <p key={i++}>{l.map(c => typeof (c) === "object" ? '@' : c).join("")}</p>)}
+			</div>
 		</div>;
 	}
 }

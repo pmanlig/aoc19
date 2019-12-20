48d54d99059b3e01f2fa54ce1898ce5e9290ecc5
pmanlig <patrik@manlig.org>
2019-12-20 06:19:48 +0100

    Begin day 20

--- src/Solvers/S20.js
+++ src/Solvers/S20.js
@@ -1,17 +1,41 @@
-// import React from 'react';
+import React from 'react';
 import Solver from './Solver';
 
 export class S20a extends Solver {
+	registerPortal(portals) { }
+
+	findDownPortal(map, x, y, portals, portalMap) {
+		let c = map[y][x];
+		if (c >= 'A' && c <= 'Z') { // Found portal
+			let port = c + map[1][x];
+			portalMap[port] = { x: x, y: 2 };
+		}
+	}
+
+	findPortals(map) {
+		let portals = map.map(l => l.map(() => undefined));
+		let portalMap = {};
+		for (let x = 0; x < map[0].length; x++) {
+			this.findDownPortal(map, x, 0, portals, portalMap);
+		}
+		for (let y = 0; y < map.length; y++) {
+		}
+	}
+
 	solve(input) {
-		this.setState({ solution: input });
+		let map = input.split("\n").map(l => l.split(""));
+		let portals = this.findPortals(map);
+		this.setState({ map: map });
+	}
+
+	customRender() {
+		let i = 0;
+		return <div style={{ fontFamily: "monospace", whiteSpace: "pre" }}>
+			{this.state.map && this.state.map.map(l => <p key={i++}>{l.join("")}</p>)}
+		</div>;
 	}
 }
 
 export class S20b extends Solver {
-	runControls = true;
-
-	solve(input) {
-		if (this.running)
-			this.setState({ solution: input });
-	}
+	static hide = true;
 }
